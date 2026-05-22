6b3df482f18f72d56cb33cf9884a31522f0948c7
thedaripanator <sayondeep1618@gmail.com>
2026-05-22 20:27:39 +0530

    Fix: Resolve relative filepath crash and improve model saving

--- model/train_model.py
+++ model/train_model.py
@@ -4,13 +4,19 @@ from sklearn.linear_model import LinearRegression
 import pickle
 
 # Load dataset
-data = pd.read_csv('../data/mess_data.csv')
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+file_path = os.path.join(BASE_DIR, 'data', 'mess_data.csv')
+
+data = pd.read_csv(file_path)
 
 X = data[['food_quality', 'cleanliness', 'quantity', 'taste']]
 y = data['rating']
 
 # Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2 ,random_state=42)
 
 # Train model
 model = LinearRegression()
@@ -21,5 +27,8 @@ score = model.score(X_test, y_test)
 print(f"Model Accuracy: {score}")
 
 # Save model
-pickle.dump(model, open('model.pkl', 'wb'))
+model_path = os.path.join(BASE_DIR, 'model', 'model.pkl')
+
+with open(model_path, 'wb') as f:
+    pickle.dump(model, f)
 print("Model saved successfully!")
